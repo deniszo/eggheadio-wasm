e8aa3f589a1b4aa457005d520611adef550638be
faliah <dezolkin@gmail.com>
2017-06-08 15:39:40 +0300

    refactor the code

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-fetch('./test.wasm')
-  .then(res => {
-    if (res.ok) return res.arrayBuffer();
-    throw new Error('Unable to fetch WASM.');
-  })
-  .then(WebAssembly.compile)
-  .then(WebAssembly.instantiate)
-  .then(instance => {
-    window.wasmSqrt = instance.exports.sqrt;
-  });
+(function init(w) {
+	testWasm(w);
+})(window);
+
+function testWasm(window) {
+	fetchAndInstantiateWasm('./test.wasm').then(storeSqrtRef);
+
+	function storeSqrtRef({ sqrt }) {
+		window.wasmSqrt = sqrt;
+	}
+}
+
+function fetchAndInstantiateWasm(url, imports = {}) {
+	return fetch(url)
+		.then(res => {
+			if (res.ok) return res.arrayBuffer();
+			throw new Error('Unable to fetch WASM.');
+		})
+		.then(WebAssembly.compile)
+		.then(module => WebAssembly.instantiate(module, imports))
+		.then(instance => instance.exports);
+}
