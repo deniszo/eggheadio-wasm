726feefd3962eed4f15a783fd8f9974af2a8f577
faliah <dezolkin@gmail.com>
2017-06-09 13:42:40 +0300

    Read WebAssembly Memory from JavaScript

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import lesson1 from './lessons/1_create_and_run_native_wasm_fn.js';
 import lesson2 from './lessons/2_compiled_c_code_into_wasm.js';
 import lesson3 from './lessons/3_calljs_fn_from_wasm.js';
+import lesson4 from './lessons/4_read_wasm_memory_from_js.js';
 
 (() => {
 	lesson1();
 	lesson2();
 	lesson3();
+	lesson4();
 })(window);
---
+++ src/lessons/4_read_wasm_memory_from_js.js
@@ -0,0 +1,11 @@
+import { fetchAndInstantiateWasm, getWasm, cGroup } from '../utils';
+
+export default () => {
+	fetchAndInstantiateWasm(getWasm('4_read_wasm_memory_from_js')).then(({ memory, getStrOffset }) => {
+		cGroup('readWasmMemoryFromJs', () => {
+			const strBuf = new Uint8Array(memory.buffer, getStrOffset(), 11);
+			const str = new TextDecoder().decode(strBuf);
+			console.log(str);
+		});
+	});
+};
